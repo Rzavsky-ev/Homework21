5b0db59a3e4b7b2b4ca5941bc9db6308613e6704
Rzavsky-ev <rzavsky.ev@gmail.com>
2024-11-26 13:47:24 +0300

    Первый коммит

---
+++ src/main/java/pro/sky/Homework21/Homework21Application.java
@@ -0,0 +1,13 @@
+package pro.sky.Homework21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework21Application.class, args);
+	}
+
+}
